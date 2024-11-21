0a4b65b6098c27d22d25fb41ba94af127f3082d3
Goblin God <37878138+esutley@users.noreply.github.com>
2024-11-21 13:38:16 -0600

    Fixing up Logic
    Fixes two logic errors (Kurt Zisa and Bear and Clock Puzzle) that could lead to unreachable locations, as well as some minor tweaks I had reported earlier

--- worlds/kh1/Rules.py
+++ worlds/kh1/Rules.py
@@ -235,6 +235,11 @@ def set_rules(kh1world):
             state.has("Progressive Glide", player)
             or
             (
+                state.has("High Jump", player, 2)
+                and state.has("Footprints", player)
+            )
+            or
+            (
                 options.advanced_logic
                 and state.has_all({
                     "High Jump",
@@ -244,6 +249,11 @@ def set_rules(kh1world):
     add_rule(kh1world.get_location("Wonderland Tea Party Garden Above Lotus Forest Entrance 1st Chest"),
         lambda state: (
             state.has("Progressive Glide", player)
+            or
+            (
+                state.has("High Jump", player, 2)
+                and state.has("Footprints", player)
+            )
             or
             (
                 options.advanced_logic
@@ -257,7 +267,6 @@ def set_rules(kh1world):
         
            state.has("Footprints", player)
            or (options.advanced_logic and state.has("Progressive Glide", player))
-           or state.has("High Jump", player, 2)
         ))
     add_rule(kh1world.get_location("Wonderland Tea Party Garden Across From Bizarre Room Entrance Chest"),
         lambda state: (
@@ -376,6 +385,7 @@ def set_rules(kh1world):
     add_rule(kh1world.get_location("Monstro Chamber 6 Other Platform Chest"),
         lambda state: (
             state.has("High Jump", player)
+            and state.has("Progressive Glide", player)
             or (options.advanced_logic and state.has("Combo Master", player))
         ))
     add_rule(kh1world.get_location("Monstro Chamber 6 Platform Near Chamber 5 Entrance Chest"),
@@ -386,6 +396,7 @@ def set_rules(kh1world):
     add_rule(kh1world.get_location("Monstro Chamber 6 Raised Area Near Chamber 1 Entrance Chest"),
         lambda state: (
             state.has("High Jump", player)
+            and state.has("Progressive Glide", player)
             or (options.advanced_logic and state.has("Combo Master", player))
         ))
     add_rule(kh1world.get_location("Halloween Town Moonlight Hill White Trinity Chest"),
@@ -594,6 +605,7 @@ def set_rules(kh1world):
         lambda state: (
             state.has("Green Trinity", player)
             and has_all_magic_lvx(state, player, 2)
+            and has_defensive_tools(state, player)
         ))
     add_rule(kh1world.get_location("Neverland Hold Flight 2nd Chest"),
         lambda state: (
@@ -709,8 +721,7 @@ def set_rules(kh1world):
         lambda state: state.has("White Trinity", player))
     add_rule(kh1world.get_location("End of the World Giant Crevasse 5th Chest"),
         lambda state: (
-            state.has("High Jump", player) 
-            or state.has("Progressive Glide", player)
+            state.has("Progressive Glide", player)
         ))
     add_rule(kh1world.get_location("End of the World Giant Crevasse 1st Chest"),
         lambda state: (
@@ -1440,10 +1451,11 @@ def set_rules(kh1world):
                 has_emblems(state, player, options.keyblades_unlock_chests)
                 and has_x_worlds(state, player, 7, options.keyblades_unlock_chests)
                 and has_defensive_tools(state, player)
+                and state.has("Progressive Blizzard", player, 3)
             ))
         add_rule(kh1world.get_location("Agrabah Defeat Kurt Zisa Zantetsuken Event"),
             lambda state: (
-                has_emblems(state, player, options.keyblades_unlock_chests) and has_x_worlds(state, player, 7, options.keyblades_unlock_chests) and has_defensive_tools(state, player)
+                has_emblems(state, player, options.keyblades_unlock_chests) and has_x_worlds(state, player, 7, options.keyblades_unlock_chests) and has_defensive_tools(state, player) and state.has("Progressive Blizzard", player, 3)
             ))
     if options.super_bosses or options.goal.current_key == "sephiroth":
         add_rule(kh1world.get_location("Olympus Coliseum Defeat Sephiroth Ansem's Report 12"),
